c649670ff8d9ecbf0528c95ece613e7958ffbbfc
Karthik Karnati <karthikkarnati6@gmail.com>
2026-07-14 20:16:32 +0530

    Add files via upload

---
+++ Ollama_bootcamp/analyser.py
@@ -0,0 +1,18 @@
+import ollama
+from prompt import PROMPT
+
+def analyze_resume(resume_text):
+
+    prompt = PROMPT.format(resume=resume_text)
+
+    response = ollama.chat(
+        model="Qwen2.5:3b",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response["message"]["content"]
---
+++ Ollama_bootcamp/prompt.py
@@ -0,0 +1,18 @@
+PROMPT = """
+
+You are an experienced HR Manager and ATS Resume Expert.
+Analyze the following resume carefully.
+Provide the output in the following format: 
+
+1. ATS Score (Out of 100)
+2. Resume Summary
+3. Strengths
+4. Weaknesses
+5. Missing Skills
+6. Improvements suggestion 
+7. Suitable job role
+8. Five inteview questions with answers
+
+Resume:
+
+"""
